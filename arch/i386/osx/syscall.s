1604689710724a44422df72b443eb2c946a80ec5
Bet4 <16643669+bet4it@users.noreply.github.com>
2018-04-06 19:10:45 +0800

    Revert "replace inline asm with standalone for syscalls"
    This reverts commit e07c45de562cd550c1cbd0b6820c66bbebee19ec.

--- arch/i386/osx/syscall.s
+++
@@ -1,5 +0,0 @@
-.global _syscall
-_syscall:
-    mov 28(%esp), %eax
-    int $0x80
-    ret
--- arch/x86_64/_syscall.s
+++
@@ -1,8 +0,0 @@
-.global syscall
-.global _syscall
-syscall:
-_syscall:
-    mov 8(%rsp), %rax
-    mov %rcx, %r10
-    syscall
-    ret
